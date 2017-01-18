a929fe20a0b91fe31bf52283b693884e3ff05ce4
akalimullah <akalimullah@users.noreply.github.com>
2017-01-18 16:05:46 -0600

    Completed the guide

--- app/controllers/programs_controller.rb
+++ app/controllers/programs_controller.rb
@@ -1,16 +1,21 @@
 class ProgramsController < ApplicationController
   def permanent_programs
     # Your code goes here.
-
-    @your_output = "Replace this string with your output"
+    lucky_number = 5
+    @your_output = lucky_number
 
     render("programs/permanent_programs.html.erb")
   end
 
   def conditionals
     # Your code goes here
-
-    @your_output = "Replace this string with your output"
+    lucky_number = rand(45)
+    if lucky_number > 10 &&  5 * lucky_number > 70
+      text = "big lucky number"
+    else
+      text = "smol lucky number"
+    end
+    @your_output = text
 
     render("programs/conditionals.html.erb")
   end
